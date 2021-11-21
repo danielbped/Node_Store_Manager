4b6148c8a45ea3fb23792e3848ccbb3ead8f96a7
Daniel Batista <danielbped@gmail.com>
2021-11-21 13:22:50 -0300

    Corrige req 3

--- controllers/products/update.js
+++ controllers/products/update.js
@@ -1,8 +1,5 @@
 const statusCode = require('http-status-codes').StatusCodes;
-const { isValidId } = require('../../middlewares/utils/validations');
 const update = require('../../services/products/update');
-const errorMessage = require('../../middlewares/errorMessage');
-const errors = require('../../utils/errorMessages');
 
 module.exports = async (req, res, next) => {
   try {
@@ -10,17 +7,13 @@ module.exports = async (req, res, next) => {
 
     const { name, quantity } = req.body;
 
-    if (!isValidId(id)) {
-      return res.status(statusCode.UNPROCESSABLE_ENTITY)
-        .json(errorMessage(errors.invalidId));
-    }
-
     const product = { id, name, quantity };
 
     const updatedProduct = await update(product);
 
     if (updatedProduct.error) {
-      return res.status(statusCode.UNPROCESSABLE_ENTITY).json(updatedProduct.error);
+      return res.status(statusCode.UNPROCESSABLE_ENTITY)
+        .json(updatedProduct.error);
     }
 
     res.status(statusCode.OK).json(product);
--- services/products/update.js
+++ services/products/update.js
@@ -1,11 +1,13 @@
 const update = require('../../models/products/update');
 const { isValidId } = require('../../middlewares/utils/validations');
 const isValidProduct = require('../../middlewares/product/isValidProduct');
+const errorMessage = require('../../middlewares/errorMessage');
+const error = require('../../utils/errorMessages');
 
 module.exports = async (product) => {
   const { id } = product;
-  if (isValidId(id).error) return isValidId(id);
-  if (isValidProduct(product).error) return isValidProduct(product);
+  if (!isValidId(id)) return errorMessage(error.invalidSale);
+  if (typeof isValidProduct(product) === 'object') return isValidProduct(product);
 
   return update(product);
 };
