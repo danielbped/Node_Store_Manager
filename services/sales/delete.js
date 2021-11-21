43615fe12482cda7928216b0f91a9f1edb979a60
Daniel Batista <danielbped@gmail.com>
2021-11-21 12:48:19 -0300

    Requisito 9

--- services/sales/delete.js
+++ services/sales/delete.js
@@ -1,8 +1,21 @@
 const deleteSale = require('../../models/sales/delete');
+const updateProduct = require('../products/update');
+const getProductById = require('../products/getById');
+const getSaleById = require('./getById');
 const { isValidId } = require('../../middlewares/utils/validations');
 
 module.exports = async (id) => {
   if (isValidId(id).error) return isValidId(id);
+
+  const { itensSold } = await getSaleById(id);
+
+  itensSold.forEach(async ({ productId, quantity }) => {
+    const { quantity: availableQuantity, name } = await getProductById(productId);
+  
+    const newQuantity = availableQuantity + quantity;
+  
+    updateProduct({ id: productId, name, quantity: newQuantity });
+  });
 
   return deleteSale(id);
 };
